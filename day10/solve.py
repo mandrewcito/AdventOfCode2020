203eab93e8f892a4e57bb6c589843d2fbe9b8f0b
mandrewcito <anbaalo@gmail.com>
2020-12-10 19:28:20 +0100

    feat(day10): alg debug docstring

--- day10/solve.py
+++ day10/solve.py
@@ -43,10 +43,43 @@ diffs = get_jolt_diffs(problem_input)
 print(f"Sol 1: {diffs[0] * diffs[2] }")
 
 def get_jolts_paths(adapters):
+    """example:
+        input => [1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22]
+        idx(jolt) => (-1) + (-2) + (-3)
+        
+        1 => 1 + 0 + 0
+        4 => 0 + 0 + 1
+        5 => 1 + 0 + 0
+        6 => 1 + 1 + 0
+        7 => 2 + 1 + 1
+        10 => 0 + 0 + 4
+        11 => 4 + 0 + 0
+        12 => 4 + 4 + 0
+        15 => 0 + 0 + 8
+        16 => 8 + 0 + 0
+        19 => 0 + 0 + 8
+        22 => 0 + 0 + 8
+
+        data => defaultdict(<class 'int'>,
+        {
+            0: 1, -1: 0, -2: 0, 1: 1, 3: 0, 2: 0, 4: 1, 5: 1, 
+            6: 2, 7: 4, 9: 0, 8: 0, 10: 4, 11: 4, 12: 8, 14: 0,
+            13: 0, 15: 8, 16: 8, 18: 0, 17: 0, 19: 8, 21: 0, 20: 0,
+            22: 8
+        })
+    Args:
+        adapters (list): jolts list
+       
+    Returns:
+        int: number of paths can be explored
+    """
+    print(list(map(lambda x: x.output_joltage, adapters)))
     data = collections.defaultdict(int)
     data[0] = 1
     for adapter in adapters: 
+        print(f"{adapter.output_joltage} => {data[adapter.output_joltage-1]} + {data[adapter.output_joltage-2]} + {data[adapter.output_joltage-3]}")
         data[adapter.output_joltage] = data[adapter.output_joltage-1] + data[adapter.output_joltage-2] + data[adapter.output_joltage-3] 
+    print(data)
     return data[adapters[-1].output_joltage]
 
 def get_different_paths(adapters):
